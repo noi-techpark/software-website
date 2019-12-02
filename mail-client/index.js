e82fbc35adb46da476619042fc2c3c811e1d8ad3
bertolla <p.bertolla@noi.bz.it>
2019-12-02 11:36:55 +0100

    fix contact endpoint

--- mail-client/index.js
+++ mail-client/index.js
@@ -1,4 +1,5 @@
 'user strict';
+
 var express = require("express");
 var nodemailer = require('nodemailer');
 const fs = require('fs');
@@ -7,14 +8,16 @@ var app = express();
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+
 let config = JSON.parse(fs.readFileSync('config.json')); 
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
+
 app.post('/service',function(request,response){
     if (!isValidRequest(request)){
-        response.status(400).send("You shall not pass"); 
+        response.status(400).send("You shall not pass");
     }
     else{
         console.log("Service request starts now");
@@ -29,16 +32,21 @@ app.post('/service',function(request,response){
         response.send("Service request completed");
     }
 });
+
 app.post('/contact',function(request,response){
-    console.log("Contact request starts now");
-    transporter.sendMail(config.mailOptions, function(error, info){
-        if(error){
-            return console.log(error);
-        }
-        console.log('Message sent: ' + info.response);
-    });
-    response.send("Contact request completed");
+    if (!isValidRequest(request)){
+        response.status(400).send("You shall not pass");
+    }
+    else{
+        transporter.sendMail(config.mailOptions, function(error, info){
+            if(error){
+                return console.log(error);
+            }
+        });
+        response.send("Contact request completed");
+    }
 });
+
 var transporter = nodemailer.createTransport(config.serverConfig);
 var isValidRequest = function(req){
    //return req.headers['Referer'] === "freesoftwarelab.noi.bz.it";
@@ -47,8 +55,16 @@ var isValidRequest = function(req){
 var createServiceMail = function(b){
     var text = '';
     text += b.name + ' ' + b.surname + '\n';
-    text += b.company + '\n';
+    text += (b.company===undefined) ? '' : (b.company + '\n');
     text += b.email + ' ' + b.phone + '\n';
     text += b.date1 + ', ' + b.date2 + ', ' + b.date3;
+    return text;
+}
+var createContactMail =function(b){
+    var text = '';
+    text += b.name + ' ' + b.surname + '\n';
+    text += (b.company===undefined) ? '' : (b.company + '\n');
+    text += b.email + ' ' + b.phone||'' + '\n';
+    text += b.message;
     return text;
 }
