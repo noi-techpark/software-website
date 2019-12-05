7614e05375c9f78819a4b90aaaa3cc7d3fee7936
bertolla <p.bertolla@noi.bz.it>
2019-12-05 09:57:37 +0100

    fix paths

--- mail-client/Jenkinsfile-Test.groovy
+++ mail-client/Jenkinsfile-Test.groovy
@@ -25,11 +25,11 @@ pipeline {
                 sh "echo '' > .env"
                 sh "echo 'DOCKER_IMAGE_APP=${DOCKER_IMAGE_APP}' >> .env"
                 sh "echo 'DOCKER_TAG_APP=${DOCKER_TAG_APP}' >> .env"
-                sh '''jq '.serverconfig.auth.user="'${SMTP_USER}'"' config.json > tmpFile && mv tmpFile config.json'''
-                sh '''jq '.serverconfig.auth.user="'${SMTP_PASSWORD}'"' config.json > tmpFile && mv tmpFile config.json'''
-                sh '''jq '.serverconfig.auth.user="'${SMTP_USER}'"' config.json > tmpFile && mv tmpFile config.json'''
-                sh '''jq '.mailOptions.from="'${MAIL_FROM}'"' config.json > tmpFile && mv tmpFile config.json'''
-                sh '''jq '.mailOptions.to="'${MAIL_TO}'"' config.json > tmpFile && mv tmpFile config.json'''
+                sh '''jq '.serverconfig.auth.user="'${SMTP_USER}'"' mail-client/config.json > tmpFile && mv tmpFile mail-client/config.json'''
+                sh '''jq '.serverconfig.auth.user="'${SMTP_PASSWORD}'"' mail-client/config.json > tmpFile && mv tmpFile mail-client/config.json'''
+                sh '''jq '.serverconfig.auth.user="'${SMTP_USER}'"' mail-client/config.json > tmpFile && mv tmpFile mail-client/config.json'''
+                sh '''jq '.mailOptions.from="'${MAIL_FROM}'"' mail-client/config.json > tmpFile && mv tmpFile mail-client/config.json'''
+                sh '''jq '.mailOptions.to="'${MAIL_TO}'"' mail-client/config.json > tmpFile && mv tmpFile mail-client/config.json'''
             }
         }
         stage('Build & Push') {
