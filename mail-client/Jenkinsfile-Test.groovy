967c8e6dde88f3571465a663e0059299cc7202ce
Alex Lanz <alex@krumer.it>
2020-01-07 09:41:46 +0100

    Adjusting project name strategy for Docker

--- mail-client/Jenkinsfile-Test.groovy
+++ mail-client/Jenkinsfile-Test.groovy
@@ -4,12 +4,12 @@ pipeline {
     environment {
         AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
         AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+        DOCKER_PROJECT_NAME = "software-mail-client"
         DOCKER_IMAGE_APP = "755952719952.dkr.ecr.eu-west-1.amazonaws.com/software.bz.it.mail-client"
         DOCKER_TAG_APP = "test-$BUILD_NUMBER"
         DOCKER_SERVICES = "app"
         DOCKER_SERVER_IP = "63.33.73.203"
         DOCKER_SERVER_DIRECTORY = "/var/docker/software.bz.it.mail-client"
-        DOCKER_SERVER_PROJECT = "software-mail-client"
         SMTP_USER=credentials('software.bz.it.smtp.auth.user')
         SMTP_PASSWORD=credentials('software.bz.it.smtp.auth.password')
         MAIL_FROM=credentials('software.bz.it.mail.from')
@@ -20,6 +20,7 @@ pipeline {
         stage('Configure') {
             steps {
                 sh "echo '' > .env"
+                sh "echo 'COMPOSE_PROJECT_NAME=${DOCKER_PROJECT_NAME}' >> .env"
                 sh "echo 'DOCKER_IMAGE_APP=${DOCKER_IMAGE_APP}' >> .env"
                 sh "echo 'DOCKER_TAG_APP=${DOCKER_TAG_APP}' >> .env"
                 sh '''jq '.serverConfig.auth.user="'${SMTP_USER}'"' mail-client/config.json > tmpFile && mv tmpFile mail-client/config.json'''
@@ -45,11 +46,11 @@ pipeline {
                     sh "ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'mkdir -p ${DOCKER_SERVER_DIRECTORY}/releases/${BUILD_NUMBER}'"
                     sh "cd mail-client && pv docker-compose.run.yml | ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'tee ${DOCKER_SERVER_DIRECTORY}/releases/${BUILD_NUMBER}/docker-compose.yml'"
                     sh "pv .env | ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'tee ${DOCKER_SERVER_DIRECTORY}/releases/${BUILD_NUMBER}/.env'"
-                    sh "ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'cd ${DOCKER_SERVER_DIRECTORY}/releases/${BUILD_NUMBER} && docker-compose --project-name=${DOCKER_SERVER_PROJECT} pull'"
+                    sh "ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'cd ${DOCKER_SERVER_DIRECTORY}/releases/${BUILD_NUMBER} && docker-compose pull'"
 
-                    sh "ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} '[ -d \"${DOCKER_SERVER_DIRECTORY}/current\" ] && (cd ${DOCKER_SERVER_DIRECTORY}/current && docker-compose --project-name=${DOCKER_SERVER_PROJECT} down) || true'"
+                    sh "ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} '[ -d \"${DOCKER_SERVER_DIRECTORY}/current\" ] && (cd ${DOCKER_SERVER_DIRECTORY}/current && docker-compose down) || true'"
                     sh "ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'ln -sfn ${DOCKER_SERVER_DIRECTORY}/releases/${BUILD_NUMBER} ${DOCKER_SERVER_DIRECTORY}/current'"
-                    sh "ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'cd ${DOCKER_SERVER_DIRECTORY}/current && docker-compose --project-name=${DOCKER_SERVER_PROJECT} up --detach'"
+                    sh "ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'cd ${DOCKER_SERVER_DIRECTORY}/current && docker-compose up --detach'"
                 }
             }
 	    }
