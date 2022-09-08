2bc33b85e7081ed59a67d6015fd958df384451a2
Simon Dalvai <s.dalvai@noi.bz.it>
2022-09-08 13:04:21 +0200

    add examples and fix mobile view

--- src/themes/free-software-lab/static/js/trends.js
+++ src/themes/free-software-lab/static/js/trends.js
@@ -1,1 +1,32 @@
-console.log("hello world");
+
+let generateImage = function (prompt) {
+    console.log("generate image");
+    let input = document.getElementById("prompt");
+    if (prompt != undefined) {
+        console.log("example");
+
+        input.value = prompt
+
+        let generatedImage = document.getElementById("generated_image").value;
+
+        let src = 'https://api.lorem.space/image?w=1024&h=512';
+        let img = document.createElement('img');
+
+        img.src = src
+        generateImage.style = "background-image: url(https://api.lorem.space/image?w=1024&h=512)"
+    }
+    else {
+        console.log("user");
+        console.log(prompt.value)
+
+        let generatedImage = document.getElementById("generated_image").value;
+
+        let src = 'https://api.lorem.space/image?w=1024&h=512';
+        let img = document.createElement('img');
+
+        img.src = src
+        generateImage.style = "background-image: url(https://api.lorem.space/image?w=1024&h=512)"
+    }
+
+
+}
