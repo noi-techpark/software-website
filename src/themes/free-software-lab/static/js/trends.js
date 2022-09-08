4bc9c28bc424bedba8677c12a37454a4607b3cd1
Simon Dalvai <s.dalvai@noi.bz.it>
2022-09-08 14:24:10 +0200

    add hCapctha

--- src/themes/free-software-lab/static/js/trends.js
+++ src/themes/free-software-lab/static/js/trends.js
@@ -1,13 +1,13 @@
+let hcaptchaToken = null;
 
 let generateImage = function (prompt) {
     console.log("generate image");
+    console.log(hcaptchaToken)
     let input = document.getElementById("prompt");
     if (prompt != undefined) {
         console.log("example");
 
         input.value = prompt
-
-        let generatedImage = document.getElementById("generated_image").value;
 
         let src = 'https://api.lorem.space/image?w=1024&h=512';
         let img = document.createElement('img');
@@ -19,14 +19,18 @@ let generateImage = function (prompt) {
         console.log("user");
         console.log(prompt.value)
 
-        let generatedImage = document.getElementById("generated_image").value;
-
         let src = 'https://api.lorem.space/image?w=1024&h=512';
         let img = document.createElement('img');
 
         img.src = src
         generateImage.style = "background-image: url(https://api.lorem.space/image?w=1024&h=512)"
     }
+}
 
+function captchaVerify (token){
+    hcaptchaToken = token
+}
 
+function captchaExpiered (){
+    hcaptchaToken = null
 }
