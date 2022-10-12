439b8866d7a338c6116d0be4f8edf81588aea90c
Simon Dalvai <s.dalvai@noi.bz.it>
2022-10-12 15:50:10 +0200

    s-d: remove test tokens

--- src/themes/free-software-lab/static/js/trends.js
+++ src/themes/free-software-lab/static/js/trends.js
@@ -30,9 +30,8 @@ const generateImage = async function (type, examplePrompt = undefined) {
     try {
         const res = await fetch(url);
         console.log(res);
-        // const imageToken = await res.json();
-
-        const imageToken = "test-" + type;
+        const imageToken = await res.json();
+        // const imageToken = "test-" + type;
         await pollStatus(imageToken, amount);
     } catch (e) {
         console.error("addJob error", e);
