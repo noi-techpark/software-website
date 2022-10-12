034e7f07d1edfc99fc29205d4d9d8965ecce8ed2
Simon Dalvai <s.dalvai@noi.bz.it>
2022-10-12 15:40:32 +0200

    s-d: show generated image and image gallery

--- src/themes/free-software-lab/static/js/trends.js
+++ src/themes/free-software-lab/static/js/trends.js
@@ -1,5 +1,6 @@
 const API_URL = "https://api.1006.org/524e45354e44326a67750a/0.95/";
-const GET_STATUS_POLL_TIMEOUT = 2000
+const S3_URL = "http://noi-sd.s3-website-eu-west-1.amazonaws.com/";
+const GET_STATUS_POLL_TIMEOUT = 1000
 
 let hcaptchaToken = null;
 
@@ -9,6 +10,7 @@ const generateImage = async function (type, examplePrompt = undefined) {
     console.log('generate image');
     console.log(hcaptchaToken)
 
+    showProgress();
 
     const promptInput = document.getElementById('prompt');
     const prompt = promptInput.value
@@ -28,15 +30,17 @@ const generateImage = async function (type, examplePrompt = undefined) {
     try {
         const res = await fetch(url);
         console.log(res);
-        const imageToken = await res.json();
-        await pollStatus(imageToken);
+        // const imageToken = await res.json();
+
+        const imageToken = "test-" + type;
+        await pollStatus(imageToken, amount);
     } catch (e) {
         console.error("addJob error", e);
         resetProgress();
     }
 }
 
-async function pollStatus(token) {
+async function pollStatus(token, amount) {
     const url = API_URL + `getJobStatus?token=${token}`;
 
     let response;
@@ -52,41 +56,74 @@ async function pollStatus(token) {
 
     if (response && response["State"] == "complete") {
         // generating finished and showing image
-        showImages(response["ImageUrl"]);
+        showImages(token, amount);
     } else {
         updateState(response["Age"], response["QueueLength"]);
-        setTimeout(pollStatus, GET_STATUS_POLL_TIMEOUT, token);
+        setTimeout(pollStatus, GET_STATUS_POLL_TIMEOUT, token, amount);
     }
 }
 
 function updateState(age, queueLength) {
     let progressBar = document.getElementById("progress_bar");
+    let progressBarGallery = document.getElementById("progress_bar_gallery");
     let progressStatus = document.getElementById("progress_status");
 
     age = Math.ceil(age)
-    
+
     let expectedWaitTime = ((6 * 7 * queueLength) + (6 * 7))
     // update progress bar using average 7 images and 6 seconds for every image generation in the queue and itself
     let width = (age) / ((6 * 7 * queueLength) + (6 * 7)) * 100;
     // max 100
     width = width > 100 ? 100 : width
-    
+
     console.log("progress bar witdh: " + width + "%");
     progressBar.style.width = width + "%";
+    progressBarGallery.style.width = width + "%";
 
     if (queueLength > 1)
-        progressStatus.innerHTML = `${queueLength} people are currently in the queue.<br>Expected wait time is ${expectedWaitTime}.<br>Waiting since ${age} seconds.`;
+        progressStatus.innerHTML = `${queueLength} other jobs are currently in the queue. Ready in ${expectedWaitTime} seconds`;
     else if (queueLength == 1)
-        progressStatus.innerHTML = `1 person are currently in the queue.<br>Expected wait time is ${expectedWaitTime}.<br>Waiting since ${age} seconds.`;
+        progressStatus.innerHTML = `1 other job is currently in the queue ${age}/${expectedWaitTime}. Ready in ${expectedWaitTime} seconds`;
     else
-        progressStatus.innerHTML = `Generating your image now...<br>Expected wait time is ${expectedWaitTime}.<br>Waiting since ${age} seconds.`;
+        progressStatus.innerHTML = `Generating your images now...`;
 
 }
 
-function showImages(url) {
-    console.log(`show images from ${url} coming soon...`);
+function showImages(token, amount) {
     // reset progress bar again
     resetProgress();
+    hideProgress();
+
+    // image
+    showImage(token, 0)
+
+    // gallery
+    for (let i = 0; i < amount; i++) {
+        let galleryImage = document.createElement("img");
+
+        galleryImage.setAttribute("src", `http://noi-sd.s3-website-eu-west-1.amazonaws.com/${token}/${i}.png`);
+        galleryImage.setAttribute("height", "90");
+        galleryImage.setAttribute("width", "60");
+        galleryImage.setAttribute("class", "gallery_image");
+        galleryImage.setAttribute("alt", `Generated image number ${i}`);
+        galleryImage.setAttribute("onclick", `showImage("${token}",${i})`);
+        document.getElementById("generated_gallery").appendChild(galleryImage);
+    }
+}
+
+function showImage(token, id) {
+    // remove image if exist
+    if (document.getElementById("generated_image"))
+        document.getElementById("generated_image").remove();
+
+    // show image
+    let image = document.createElement("img");
+    image.setAttribute("src", `http://noi-sd.s3-website-eu-west-1.amazonaws.com/${token}/${id}.png`);
+    image.setAttribute("height", "448");
+    image.setAttribute("width", "986");
+    image.setAttribute("id", "generated_image");
+    image.setAttribute("alt", `Generated image number 0`);
+    document.getElementById("generated_image_container").appendChild(image);
 }
 
 function captchaVerify(token) {
@@ -114,9 +151,33 @@ function captchaExpiered() {
     document.getElementById('example_button_4').disabled = true
 }
 
-function resetProgress(){
+function resetProgress() {
     let progressBar = document.getElementById("progress_bar");
+    let progressBarGallery = document.getElementById("progress_bar_gallery");
     let progressStatus = document.getElementById("progress_status");
     progressBar.style.width = "0%";
+    progressBarGallery.style.width = "0%";
     progressStatus.innerHTML = "";
+}
+
+function resetImages() {
+    // empty the gallery
+    const galleryImages = document.querySelectorAll('.gallery_image');
+    galleryImages.forEach(galleryImage => {
+        galleryImage.remove();
+    });
+
+    // remove generated image
+    if (document.getElementById("generated_image"))
+        document.getElementById("generated_image").remove();
+}
+
+function hideProgress() {
+    document.getElementById("progress_bar_gallery").style.display = 'none';
+    document.getElementById("progress_bar").style.display = 'none';
+}
+
+function showProgress() {
+    document.getElementById("progress_bar_gallery").style.display = 'block';
+    document.getElementById("progress_bar").style.display = 'block';
 }
